ed71c7e367fbc31a5d78a6bf6a50664b4a41ba8b
JasonX <jasoncwj@outlook.com>
2020-05-23 21:35:37 +0800

    Some sample files

---
+++ OLD STUFF/testClass.py
@@ -0,0 +1,7 @@
+class MyClass:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+    def myfunc(self):
+        print("Hello my name is " + self.name)
