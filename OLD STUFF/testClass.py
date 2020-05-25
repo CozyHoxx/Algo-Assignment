59c75ec0f21c7d5a7ced2d0bb1a479c071af9ff9
JasonX <jasoncwj@outlook.com>
2020-05-25 18:59:17 +0800

    Deleted old files

--- OLD STUFF/testClass.py
+++
@@ -1,7 +0,0 @@
-class MyClass:
-    def __init__(self, name: str, age: int):
-        self.name = name
-        self.age = age
-
-    def myfunc(self):
-        print("Hello my name is " + self.name)
