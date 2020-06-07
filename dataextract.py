193b705ad4eb58b4329b45b7082a6fc61c472483
JasonX <jasoncwj@outlook.com>
2020-06-07 21:28:24 +0800

    Replaced transport_objects with networkx library instead.

--- dataextract.py
+++ dataextract.py
@@ -1,60 +1,103 @@
 import pandas
-import transport_objects as tr
-
-# pandas.set_option('display.expand_frame_repr', False)
-
+# import transport_objects as tr
+import networkx as nx
+from geopy import distance
+from enum import Enum
 
 # Prototype data parser
 # Parse data from an excel file and create a list of route
 # for now its just a bus route.
 
+G = nx.DiGraph()
 
-def generate_bus_route():
+
+class Transport(Enum):
+    NULL = -1
+    BUS = 1
+    TRAIN = 2
+    AIRPLANE = 3
+    WALK = 4
+
+
+def generate_bus_route(graph: nx.Graph):
     data = pandas.read_excel('Dataset\\MRT Bus Route.xlsx')
     df = pandas.DataFrame(data, columns=['Bus Stop Name', 'Latitud', 'Longitud', 'ROUTE ID', 'Route Name', ])
 
-    bus_route_list = []
+    # bus_route_list = []
     current_route = ""
     stop = -1
-
+    prev_lat = 0.0
+    prev_lon = 0.0
     # This file is sorted according to routes, so we just check if the row is referring to the same route each loop.
     for i in df.index:
+        lat = df['Latitud'][i]
+        lon = df['Longitud'][i]
+        stop_name = df['Bus Stop Name'][i]
         # We insert a new route if its a new one
         if current_route != df['Route Name'][i]:
             current_route = df['Route Name'][i]
-            stop += 1
-            bus_route_list.append(
-                tr.TransportObject(df['Route Name'][i],
-                                   tr.Transport.BUS,
-                                   [df['Bus Stop Name'][i], df['Latitud'][i], df['Longitud'][i]]
-                                   )
-            )
+            G.add_node((lat, lon), stop_name=stop_name, route_name=current_route)
         else:
             # We keep adding new stops to the current route.
-            bus_route_list[stop].add_stops(df['Bus Stop Name'][i], df['Latitud'][i], df['Longitud'][i])
+            G.add_node((lat, lon), stop_name=stop_name, route_name=current_route)
+            G.add_edge((prev_lat, prev_lon), (lat, lon), route_name=current_route, type=Transport.BUS,
+                       weight=distance.distance((lat, lon), (prev_lat, prev_lon)).kilometers)
+        prev_lat = lat
+        prev_lon = lon
 
-    return bus_route_list
 
-
-def generate_train_route():
+def generate_train_route(graph: nx.Graph):
     data = pandas.read_excel('Dataset\\Train Route.xlsx')
     df = pandas.DataFrame(data, columns=['Stop Name', 'Latitud', 'Longitud', 'ROUTE ID', 'Route Name', ])
 
-    train_route_list = []
     current_route = ""
     stop = -1
+    prev_lat = 0.0
+    prev_lon = 0.0
 
     for i in df.index:
+        lat = df['Latitud'][i]
+        lon = df['Longitud'][i]
+        stop_name = df['Stop Name'][i]
         if current_route != df['Route Name'][i]:
             current_route = df['Route Name'][i]
-            stop += 1
-            train_route_list.append(
-                tr.TransportObject(df['Route Name'][i],
-                                   tr.Transport.TRAIN,
-                                   [df['Stop Name'][i], df['Latitud'][i], df['Longitud'][i]]
-                                   )
-            )
+            G.add_node((lat, lon), stop_name=stop_name, route_name=current_route)
         else:
-            train_route_list[stop].add_stops(df['Stop Name'][i], df['Latitud'][i], df['Longitud'][i])
+            # We keep adding new stops to the current route.
+            G.add_node((lat, lon), stop_name=stop_name, route_name=current_route)
+            G.add_edge((prev_lat, prev_lon), (lat, lon), route_name=current_route, type=Transport.TRAIN,
+                       weight=distance.distance((lat, lon), (prev_lat, prev_lon)).kilometers)
+        prev_lat = lat
+        prev_lon = lon
 
-    return train_route_list
+
+def generate_walking_route():
+    count = 0
+    for curr, curr_data in G.nodes(data=True):
+        for neigh, neigh_data in G.nodes(data=True):
+            dist = distance.distance((curr[0], curr[1]), (neigh[0], neigh[1])).kilometers
+            if dist < 0.3:
+                if (curr_data['route_name'] != neigh_data['route_name']) & (not G.has_edge(curr, neigh)):
+                    G.add_edge((curr[0], curr[1]), (neigh[0], neigh[1]), route_name='walk', type=Transport.WALK,
+                           weight=dist)
+                    count+=1
+                    print("Added" + str(count) + "Distance = " + str(dist))
+
+
+def get_graph():
+    return G
+
+
+generate_bus_route(G)
+generate_train_route(G)
+for n, data in G.nodes(data=True):
+    print(n)
+    print(data['stop_name'])
+    print(data['route_name'])
+generate_walking_route()
+for u, v, a in G.edges(data=True):
+    print(str(u) + "->" + str(v))
+    print(str(a['weight']) + "KM")
+
+print(G.nodes)
+print(G.edges)
--- map_creator.py
+++ map_creator.py
@@ -5,8 +5,10 @@ import transport_objects as tr
 from pyllist import sllist, sllistnode
 import os
 
-bus_route_list = dat.generate_bus_route()
-train_route_list = dat.generate_train_route()
+# bus_route_list = dat.generate_bus_route()
+# train_route_list = dat.generate_train_route()
+routes = dat.get_graph()
+
 
 def generate_map():
     gmap = gmplot.GoogleMapPlotter(3.150447, 101.749015, 13)
@@ -19,19 +21,32 @@ def generate_map():
     colours = itertools.cycle(['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w'])
     # Generate location of all stations.
 
-    for seq in (bus_route_list, train_route_list):
-        current_colour = next(colours)
-        for route in seq:
-            station_lats = []
-            station_lons = []
-            for stop in route.stops:
-                station_lats.append(stop[1])
-                station_lons.append(stop[2])
-            gmap.scatter(station_lats, station_lons, '#3B0B39', size=40, marker=False)
-            gmap.plot(station_lats, station_lons, current_colour, edge_width=5)
-            gmap.marker(route.firstStation[1], route.firstStation[2], current_colour)
-            gmap.marker(route.lastStation[1], route.lastStation[2], current_colour)
+    # for seq in (bus_route_list, train_route_list):
+    #     current_colour = next(colours)
+    #     for route in seq:
+    #         station_lats = []
+    #         station_lons = []
+    #         for stop in route.stops:
+    #             station_lats.append(stop[1])
+    #             station_lons.append(stop[2])
+    #         gmap.scatter(station_lats, station_lons, '#3B0B39', size=40, marker=False)
+    #         gmap.plot(station_lats, station_lons, current_colour, edge_width=5)
+    #         gmap.marker(route.firstStation[1], route.firstStation[2], current_colour)
+    #         gmap.marker(route.lastStation[1], route.lastStation[2], current_colour)
 
+    # Mark locations first
+    print(routes.nodes())
+    lat, lon = map(list, zip(*routes.nodes))
+    gmap.scatter(lat, lon, '#000000', size=40, marker=False)
+    for u, v, data in routes.edges(data=True):
+        color = ''
+        if data['type'] == dat.Transport.BUS:
+            color = 'r'
+        elif data['type'] == dat.Transport.WALK:
+            color = 'y'
+        else:
+            color = 'b'
+        gmap.plot([u[0],v[0]], [u[1],v[1]], color, edge_width=5)
     # Marker
     hidden_gem_lat, hidden_gem_lon = 3.171593, 101.693305
     gmap.marker(hidden_gem_lat, hidden_gem_lon, "cornflowerblue")
@@ -65,18 +80,18 @@ def generate_route(start_pos, end_pos):
         else:
             journey_list.append(tr.JourneyPoint(route[1], route[2], bus_route_list[0].type))
 
-
     # Draw out the lines
 
     for node in journey_list.iternodes():
         if node != journey_list.last:
             if node.next.value.type == tr.Transport.WALK:
                 gmap.marker(node.next.value.lat, node.next.value.lon, 'b')
-                gmap.plot([node.value.lat, node.next.value.lat], [node.value.lon, node.next.value.lon], 'b', edge_width=5)
+                gmap.plot([node.value.lat, node.next.value.lat], [node.value.lon, node.next.value.lon], 'b',
+                          edge_width=5)
             elif node.next.value.type == tr.Transport.BUS:
                 gmap.marker(node.next.value.lat, node.next.value.lon, 'g')
-                gmap.plot([node.value.lat, node.next.value.lat], [node.value.lon, node.next.value.lon], 'g', edge_width=5)
-
+                gmap.plot([node.value.lat, node.next.value.lat], [node.value.lon, node.next.value.lon], 'g',
+                          edge_width=5)
 
     # gmap.scatter(journey_lats, journey_lons, '#3B0B39', size=40, marker=False)
     # gmap.plot(journey_lats, journey_lons, 'b', edge_width=5)
@@ -85,3 +100,6 @@ def generate_route(start_pos, end_pos):
 
     # Draw
     gmap.draw('templates\\map.html')
+
+
+generate_map()
