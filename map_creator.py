9a7023fd44541628e57f63489179bb158afd4981
Jason Pang <jia.qing@live.com>
2020-06-12 23:56:38 +0800

    Changed formatting

--- map_creator.py
+++ map_creator.py
@@ -8,85 +8,58 @@ import os
 # bus_route_list = dat.generate_bus_route()
 # train_route_list = dat.generate_train_route()
 routes = dat.get_graph()
-gmap = gmplot.GoogleMapPlotter(3.150447, 101.749015, 13)
+gmap = gmplot.GoogleMapPlotter(3.147447, 101.655015, 13)
 
 
 def generate_map():
     # PATCH FOR RENDERING MARKERS! DO NOT TOUCH.
     gmap.coloricon = "http://www.googlemapsmarkers.com/v1/%s/"
-    # data here
-
-    # bus_route_list = []
 
     colours = itertools.cycle(['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w'])
-    # Generate location of all stations.
 
-    # for seq in (bus_route_list, train_route_list):
-    #     current_colour = next(colours)
-    #     for route in seq:
-    #         station_lats = []
-    #         station_lons = []
-    #         for stop in route.stops:
-    #             station_lats.append(stop[1])
-    #             station_lons.append(stop[2])
-    #         gmap.scatter(station_lats, station_lons, '#3B0B39', size=40, marker=False)
-    #         gmap.plot(station_lats, station_lons, current_colour, edge_width=5)
-    #         gmap.marker(route.firstStation[1], route.firstStation[2], current_colour)
-    #         gmap.marker(route.lastStation[1], route.lastStation[2], current_colour)
-
+    # Generate location of all stations
     # Mark locations first
-
     lat, lon = map(list, zip(*routes.nodes))
-    gmap.scatter(lat, lon, '#000000', size=40, marker=False)
+    gmap.scatter(lat, lon, '#939393', size=40, marker=False)
+
     for u, v, data in routes.edges(data=True):
-        color = ''
         if data['type'] == dat.Transport.BUS:
             color = 'r'
         elif data['type'] == dat.Transport.WALK:
             color = 'y'
+        elif data['type'] == dat.Transport.TRAIN:
+            color = 'b'
+        elif data['type'] == dat.Transport.AIRPLANE:
+            color = 'k'
         else:
             color = 'b'
-        gmap.plot([u[0], v[0]], [u[1], v[1]], color, edge_width=5)
-
-    start_pos = (3.12986, 101.64247)
-    end_pos = (3.128304, 101.64286)
-    gmap.marker(start_pos[0], start_pos[1], "red", title="START")
-    gmap.marker(end_pos[0], end_pos[1], "red", title="END")
+        gmap.scatter([u[0], v[0]], [u[1], v[1]], color, edge_width=5)
+        gmap.plot([u[0], v[0]], [u[1], v[1]], color, edge_width=2)
 
     # Draw
     gmap.draw('templates\\map.html')
 
 
 def generate_route():
-    # os.remove('templates\\map.html')
+    # start_pos [lat,lon] and end_pos [lat,lon]
     start_pos = (3.128440, 101.650146)  # FSKTM
-    # start_pos = (3.119611, 101.643204) # MRT Phileo Station
-    # end_pos = (3.128304, 101.64286)  # MRT Phileo Station
     end_pos = (3.13732, 101.68734)  # MRT PB Daman
 
-    # start_pos [lat,lon] and end_pos [lat,lon]
-    # gmap = gmplot.GoogleMapPlotter(start_lat, start_lon, 13)
     # PATCH FOR RENDERING MARKERS! DO NOT TOUCH.
     gmap.coloricon = "http://www.googlemapsmarkers.com/v1/%s/"
-    gmap.marker(start_pos[0], start_pos[1], 'r', title="START")
-    gmap.marker(end_pos[0], end_pos[1], 'r', title="END")
+    gmap.marker(start_pos[0], start_pos[1], 'w', title="START")
+    gmap.marker(end_pos[0], end_pos[1], 'w', title="END")
     # path finding logic may go here
     # We start by connecting the start and end position to any neighbour nodes around it (at LEAST one)
 
     path = dat.generate_path(start_pos, end_pos)
     lat, lon = map(list, zip(*path))
-    # PLACEHOLDER ROUTE
-    # generate_map()
-    # Draw out the lines
 
-    # gmap.scatter(journey_lats, journey_lons, '#3B0B39', size=40, marker=False)
-    gmap.plot(lat, lon, 'g', edge_width=5)
-    # gmap.marker(journey_lats[0], journey_lons[0], 'g')
-    # gmap.marker(journey_lats[len(journey_lats) - 1], journey_lons[len(journey_lons) - 1], 'r')
+    gmap.plot(lat, lon, 'w', edge_width=5)
 
     # Draw
     gmap.draw('templates\\map.html')
 
 
 generate_map()
-generate_route()
+# generate_route()
