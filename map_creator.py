76387dbcaeaf018434ce02a59a9d451dd97d580e
JasonX <jasoncwj@outlook.com>
2020-06-22 17:42:37 +0800

    Fixed route text order and display.

--- map_creator.py
+++ map_creator.py
@@ -53,16 +53,16 @@ def generate_route():
     start_pos = (6.3322, 99.7322)  # FSKTM
     end_pos = (1.5818, 103.654083)  # MRT PB Daman
     colours = itertools.cycle(['#0275d8', '#5cb85c', '#f0ad4e', '#d9534f', '#292b2c'])
-    gmap = gmplot.GoogleMapPlotter(3.147447, 101.655015, 7)
-    # PATCH FOR RENDERING MARKERS! DO NOT TOUCH.
-    gmap.coloricon = "http://www.googlemapsmarkers.com/v1/%s/"
-    gmap.marker(start_pos[0], start_pos[1], 'w', title="START")
-    gmap.marker(end_pos[0], end_pos[1], 'w', title="END")
-    # path finding logic may go here
-    # We start by connecting the start and end position to any neighbour nodes around it (at LEAST one)
+    new_route_gmap = gmplot.GoogleMapPlotter(3.147447, 101.655015, 7)
 
-    route_subgraph = dat.generate_path(start_pos, end_pos)
-    route_list = []
+    # PATCH FOR RENDERING MARKERS! DO NOT TOUCH.
+    new_route_gmap.coloricon = "http://www.googlemapsmarkers.com/v1/%s/"
+    new_route_gmap.marker(start_pos[0], start_pos[1], 'w', title="START")
+    new_route_gmap.marker(end_pos[0], end_pos[1], 'w', title="END")
+
+
+    route_subgraph, route_list = dat.generate_path(start_pos, end_pos)
+
     for curr_route in route_subgraph:
         print("NEXT ROUTE")
         print(curr_route.edges())
@@ -72,27 +72,16 @@ def generate_route():
             curr_color = next(colours)
             stop1 = routes.nodes[u]['stop_name']
             stop2 = routes.nodes[v]['stop_name']
-            transport_type = " "
-            if data['type'] == Transport.WALK:
-                transport_type = "walking"
-            elif data['type'] == Transport.BUS:
-                transport_type = "bus"
-            elif data['type'] == Transport.TRAIN:
-                transport_type = "train"
-            elif data['type'] == Transport.AIRPLANE:
-                transport_type = "airplane"
-            print(stop1 + " to " + stop2 + " by " + str(transport_type))
-            route_string += stop1 + " to " + stop2 + " by " + str(transport_type)
-            gmap.marker(u[0], u[1], 'w', title=stop1)
-            gmap.marker(v[0], v[1], 'w', title=stop2)
+            new_route_gmap.marker(u[0], u[1], 'w', title=stop1)
+            new_route_gmap.marker(v[0], v[1], 'w', title=stop2)
             # gmap.scatter([u[0], v[0]], [u[1], v[1]], 'r', edge_width=5)
-            gmap.polygon([u[0], v[0]], [u[1], v[1]], curr_color, edge_width=2)
-        route_list.append(route_string)
+            new_route_gmap.polygon([u[0], v[0]], [u[1], v[1]], curr_color, edge_width=2)
+
 
         # gmap.plot(lat, lon, 'w', edge_width=5)
 
     # Draw
-    gmap.draw('templates\\map.html')
+    new_route_gmap.draw('templates\\map.html')
     return route_list
 
 
